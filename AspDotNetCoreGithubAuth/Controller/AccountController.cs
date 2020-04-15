0d3fac61e0e558c6f19ba56fe41b12222cd3d9ee
azy <ajeet.kumar-tiwari@agilent.com>
2020-04-15 18:32:31 +1000

    first commit

---
+++ AspDotNetCoreGithubAuth/Controller/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspDotNetCoreGithubAuth.Controller
+{
+    [Route("[controller]/[action]")]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });
+        }
+    }
+}
